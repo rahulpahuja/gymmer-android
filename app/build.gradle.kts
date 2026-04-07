9701b49b2bf4454eb097198f176eb1661b744ae1
Rahul Pahuja <10242693+rahulpahuja@users.noreply.github.com>
2026-04-07 18:56:55 +0530

    Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.parcelize)
     alias(libs.plugins.ksp)
+    id("com.google.firebase.crashlytics")
 }
 
 android {
