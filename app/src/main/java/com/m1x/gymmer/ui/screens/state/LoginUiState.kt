c6709b3315f48aed330e03b291e5b7f543f99215
Rahul Pahuja <10242693+rahulpahuja@users.noreply.github.com>
2026-04-07 18:00:34 +0530

    Login Change

--- app/src/main/java/com/m1x/gymmer/ui/screens/state/LoginUiState.kt
+++ app/src/main/java/com/m1x/gymmer/ui/screens/state/LoginUiState.kt
@@ -1,8 +1,13 @@
 package com.m1x.gymmer.ui.screens.state
 
+enum class UserRole {
+    TRAINEE, TRAINER
+}
+
 data class LoginUiState(
     val email: String = "",
     val password: String = "",
+    val selectedRole: UserRole = UserRole.TRAINEE,
     val isLoading: Boolean = false,
     val error: String? = null
 )
